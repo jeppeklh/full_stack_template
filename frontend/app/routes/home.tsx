00bf1a858653b0937c1c94bf870cb802a8cdd1b6
Jeppe Lynge Hansen <jeppeklh@hotmail.com>
2025-10-15 16:48:16 +0200

    fix: removes deprecated react router. now is normal vite

--- frontend/app/routes/home.tsx
+++ frontend/app/routes/home.tsx
@@ -1,13 +1,7 @@
-import type { Route } from "./+types/home";
+// app/routes/home.tsx
 import { Welcome } from "../welcome/welcome";
 
-export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
-}
-
 export default function Home() {
+  document.title = "Home";
   return <Welcome />;
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,8 +1,10 @@
-import { reactRouter } from "@react-router/dev/vite";
-import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
+  server: { port: 5173 }
 });
+
