7be6324d0a7d785e1ad9faed1701c17b17116f43
Jeppe Lynge Hansen <jeppeklh@hotmail.com>
2025-10-16 14:00:16 +0200

    feat: now uses data from the backend

--- frontend/app/components/personnel/PersonnelPage.tsx
+++ frontend/app/components/personnel/PersonnelPage.tsx
@@ -1,34 +1,44 @@
 import { useMemo, useState } from "react";
 import { PersonnelTable } from "./PersonnelTable";
 import { UserStatus, type Personnel } from "@/models/personnel";
+import { usePersonnelList } from "@/hooks/usePersonnel";
 
-const demo = [
-  {
-    id: "1",
-    initials: "JHH",
-    fullName: "Jens Hansen",
-    email: "jens.hansen@example.com",
-    userStatus: UserStatus.Active,
-    doctorTypeName: "Kirurg",
-  },
+// const demo = [
+//   {
+//     id: "1",
+//     initials: "JHH",
+//     fullName: "Jens Hansen",
+//     email: "jens.hansen@example.com",
+//     userStatus: UserStatus.Active,
+//     doctorTypeName: "Kirurg",
+//   },
 
-  {
-    id: "2",
-    initials: "MLL",
-    fullName: "Mette Larsen",
-    email: "mette.larsen@example.com",
-    userStatus: UserStatus.Inactive,
-    doctorTypeName: "Anæstesilæge",
-  },
-] satisfies Personnel[];
+//   {
+//     id: "2",
+//     initials: "MLL",
+//     fullName: "Mette Larsen",
+//     email: "mette.larsen@example.com",
+//     userStatus: UserStatus.Inactive,
+//     doctorTypeName: "Anæstesilæge",
+//   },
+// ] satisfies Personnel[];
 
 export function PersonnelPage() {
-  const [list] = useState(demo);
+  const { query } = usePersonnelList();
+  const personnel = query.data ?? [];
 
-  const sorted = useMemo(
-    () => [...list].sort((a, b) => a.fullName.localeCompare(b.fullName)),
-    [list]
-  );
+  //   const sorted = useMemo(
+  //     () => [...list].sort((a, b) => a.fullName.localeCompare(b.fullName)),
+  //     [list]
+  //   );
+
+  if (query.isLoading) {
+    return <div className="text-muted-foreground">Henter Personale...</div>;
+  }
+
+  if (query.isError) {
+    return <div className="text-destructive">Kunne ikke hente personale.</div>;
+  }
 
   return (
     <section className="space-y-4">
@@ -38,7 +48,7 @@ export function PersonnelPage() {
       </header>
 
       <PersonnelTable
-        items={sorted}
+        items={personnel}
         onEdit={(person) => console.log("Edit clicked", person)}
         onDeactivate={(person) => console.log("Deactivate clicked", person)}
       />
