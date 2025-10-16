86ab0c6694c135b09ac6598298a33d202411fc62
Jeppe Lynge Hansen <jeppeklh@hotmail.com>
2025-10-16 10:49:18 +0200

    feat: adds relevant shadcn-components

--- frontend/app/App.tsx
+++ frontend/app/App.tsx
@@ -3,7 +3,7 @@ import { AppShell } from "./components/layout/AppShell";
 export default function App() {
   return (
     <AppShell>
-      <div className="min-h-screen flex items-center justify-center bg-muted/10">
+      <div className="rounded-md border bg-card p-6 bg-muted/10">
         <h1 className="text-3xl font-semibold">Frontend yay!</h1>
       </div>
     </AppShell>
